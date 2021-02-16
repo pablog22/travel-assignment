56d2b91d255766cddb03b4ecabe533f67639f9d6
Pablo Grasso <pgrasso22@gmail.com>
2021-02-16 15:17:08 +0100

    Add actuator

--- src/main/java/com/afkl/travel/exercise/configuration/SecurityConfig.java
+++ src/main/java/com/afkl/travel/exercise/configuration/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.afkl.travel.exercise.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/travel").permitAll()
+                .antMatchers("/travel/**").hasAnyAuthority("USER")
+                .antMatchers("/actuator/**").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
@@ -32,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("someuser").password("{noop}psw")
-                .authorities("ROLE_USER");
+                .withUser("someuser").password("{noop}psw").authorities("USER")
+            .and()
+                .withUser("ops").password("{noop}psw").authorities("ADMIN");
     }
 }
